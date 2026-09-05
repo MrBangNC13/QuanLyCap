5cad3c97d82cd6be28d105a3ad97ccf413a30b59
MrBangNC13 <j9r744rr79@privaterelay.appleid.com>
2026-09-05 18:33:45 +0700

    Update app.py

--- app.py
+++ app.py
@@ -42,7 +42,7 @@ def load_server_data():
         return df, selected_file
     return None, None
 
-st.title("⚡ XÁC ĐỊNH VỊ TRÍ ĐỨT CÁP QUANG MAKE BY BANGNC13")
+st.title("⚡ XÁC ĐỊNH VỊ TRÍ ĐỨT CÁP  Make By BangNC13")
 st.caption("Fiber Optic Break Location Finder - FPT Telecom System")
 
 # 2. Tải Dữ Liệu Từ Server
